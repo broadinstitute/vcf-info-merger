983db5df53575bcd2e97213d46e57abf877b4165
Devin McCabe <dmccabe@broadinstitute.org>
2025-05-08 16:54:35 -0400

    set_up_gcp_friendly_logging

--- vcf_info_merger/__main__.py
+++ vcf_info_merger/__main__.py
@@ -1,4 +1,5 @@
 import logging
+import sys
 from pathlib import Path
 from typing import Annotated, Any
 
@@ -16,13 +17,41 @@ def done(*args, **kwargs):
     logging.info("Done.")
 
 
+def set_up_gcp_friendly_logging(level: int = logging.INFO) -> None:
+    """
+    Configure logging so that logs are routed to stdout/stderr based on severity,
+    for compatibility with Google Cloud Logging, and are prepended by timestamps.
+
+    :param level: log level to set
+    """
+
+    logger = logging.getLogger()
+    logger.setLevel(level)
+    logger.handlers.clear()
+
+    # formatter for all log levels
+    formatter = logging.Formatter(
+        "%(asctime)s %(levelname)s - %(message)s", "%Y-%m-%d %H:%M:%S"
+    )
+
+    # handler for DEBUG and INFO → stdout
+    stdout_handler = logging.StreamHandler(sys.stdout)
+    stdout_handler.setLevel(logging.DEBUG)
+    stdout_handler.addFilter(lambda x: x.levelno < logging.WARNING)
+    stdout_handler.setFormatter(formatter)
+
+    # handler for WARNING and above → stderr
+    stderr_handler = logging.StreamHandler(sys.stderr)
+    stderr_handler.setLevel(logging.WARNING)
+    stderr_handler.setFormatter(formatter)
+
+    logger.addHandler(stdout_handler)
+    logger.addHandler(stderr_handler)
+
+
 @app.callback(result_callback=done)
 def main():
-    logging.basicConfig(
-        format="%(asctime)s %(levelname)s - %(message)s",
-        datefmt="%Y-%m-%d %H:%M:%S",
-        level=logging.INFO,
-    )
+    set_up_gcp_friendly_logging()
 
 
 @app.command()
